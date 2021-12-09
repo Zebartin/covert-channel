dfff72b743c0f2ccc788b14e5d991186c5dcf951
TOMATOFAQ <31461318+TOMATOFAQ@users.noreply.github.com>
2021-12-09 22:16:20 +0800

    完成了字符串的收发流程

--- receiver.c
+++ receiver.c
@@ -11,13 +11,41 @@ int main()
 {
     // 初始化通信环境,与 sender 第一次握手
     int fd_A = open(FILE_A_PATH, O_CREAT, 0777);
+    struct stat s;
+    stat(FILE_A_PATH, &s);
+    int64_t last_time = s.st_atime;
     int fd_B = open(FILE_B_PATH, O_CREAT, 0777);
     int fd_C = open(FILE_C_PATH, O_CREAT, 0777);
 
-    // 等待数据发送
-    loop_until_modified(FILE_B_PATH);
-    // 发送 ack
-    my_touch(FILE_C_PATH);
+    while (1) {
+        //  同时监控 文件AB有无被修改
+        struct stat s_a, s_b;
+
+        stat(FILE_A_PATH, &s_a);
+        stat(FILE_B_PATH, &s_b);
+        printf("%s FIRST STAMP %ld\n", FILE_A_PATH, s_a.st_atime);
+        printf("%s FIRST STAMP %ld\n", FILE_B_PATH, s_b.st_atime);
+
+        stat(FILE_A_PATH, &s_a);
+        stat(FILE_B_PATH, &s_b);
+        long long last_a_time = s_a.st_atime;
+        long long last_b_time = s_b.st_atime;
+        while (last_a_time == s_a.st_atime && last_b_time == s_b.st_atime) {
+            stat(FILE_A_PATH, &s_a);
+            stat(FILE_B_PATH, &s_b);
+            printf("FILE %s HASN'T CHANGE:%ld\n", FILE_A_PATH, s_a.st_atime);
+            printf("FILE %s HASN'T CHANGE:%ld\n", FILE_B_PATH, s_a.st_atime);
+            fflush(NULL);
+            sleep(1);
+        }
+
+        // 若文件 A 被修改,说明发送的是终止信号,否则是数据
+        if (last_a_time != s_a.st_atime) {
+            break;
+        } else {
+            my_touch(FILE_C_PATH);
+        }
+    }
 
     // 销毁通信环境
     // 显式地等待 1s,以防止文件过快删除,sender 读不到最新的 st_atime
--- sender.c
+++ sender.c
@@ -7,6 +7,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+const char* buf = "hi";
+
 int main()
 {
     // 等待第一次握手以建立通信
@@ -14,15 +16,20 @@ int main()
     loop_until_exists(FILE_B_PATH);
     loop_until_exists(FILE_C_PATH);
 
-    // 由于 st_atime 的最小粒度为 1,所以此处显式等待 1s,让 receiver 获取文件B的一个有效的时间戳
-    // sleep(1);
-
     printf("START SENDING\n");
 
-    // 告知数据已发送
-    my_touch(FILE_B_PATH);
-    // 等待ack
-    loop_until_modified(FILE_C_PATH);
+    char ch;
+    int idx = 0;
+
+    ch = buf[idx++];
+    while (ch != '\0') {
+        printf("SENDING %c\n", ch);
+        ch = buf[idx++];
+        // 告知数据已发送
+        my_touch(FILE_B_PATH);
+        // 等待ack
+        loop_until_modified(FILE_C_PATH);
+    }
 
     printf("FINISH SENDING\n");
 
